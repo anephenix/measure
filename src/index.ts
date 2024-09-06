c53b90163a0b9cb589318170f79914121da14ac7
Paul Jensen <paulbjensen@gmail.com>
2024-09-06 09:12:54 +0100

    Updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { MeasureType, ValueType } from './global';
 // Interfaces
 
 interface MeasureProps {
-  type?: MeasureType
+  type?: MeasureType;
 }
 
 interface CountObject {
@@ -13,17 +13,16 @@ interface CountObject {
 // Main class
 
 class Measure {
-
   type: MeasureType;
   recordings: Array<number>;
 
-  constructor(props:MeasureProps={}) {
+  constructor(props: MeasureProps = {}) {
     this.type = props.type || 'sample';
-    this.recordings = [];
+    this.recordings = []; // This is good for in-memory storage, and when data sizes are small. Perhaps in future a database option would be good, like sqlite as a starter.
   }
 
   // Adds the recording to the list of recordings
-  record(valueOrValues:ValueType):void {
+  record(valueOrValues: ValueType): void {
     if (valueOrValues instanceof Array) {
       this.recordings.push(...valueOrValues);
     } else {
@@ -35,11 +34,11 @@ class Measure {
     Calculates the mean average of the recordings,
     or returns null if there are no recordings
   */
-  mean():null | number {
+  mean(): null | number {
     if (this.recordings.length === 0) return null;
-    const count:number = this.recordings.length;
-    const sumFunction = (total:number, recording:number) => total + recording;
-    const sum:number = this.recordings.reduce(sumFunction, 0);
+    const count: number = this.recordings.length;
+    const sumFunction = (total: number, recording: number) => total + recording;
+    const sum: number = this.recordings.reduce(sumFunction, 0);
     return sum / count;
   }
 
@@ -47,45 +46,42 @@ class Measure {
     Calculates the median value of the recordings,
     or returns null if there are no recordings
   */
-  median():null | number {
-    const length:number = this.recordings.length;
+  median(): null | number {
+    const length: number = this.recordings.length;
     if (length === 0) return null;
-    const sortFunction = (a:number,b:number) => a - b;
-    // Sort the recording in numerical order 
-    const orderedRecordings:Array<number> = this.recordings.sort(sortFunction);
+    const sortFunction = (a: number, b: number) => a - b;
+    // Sort the recording in numerical order
+    const orderedRecordings: Array<number> = this.recordings.sort(sortFunction);
     // If an even number, calculate the average of the 2 middle numbers
     if (length % 2 === 0) {
-      const firstNumber:number = orderedRecordings[(length / 2)-1]
-      const secondNumber:number = orderedRecordings[(length / 2)]
+      const firstNumber: number = orderedRecordings[length / 2 - 1];
+      const secondNumber: number = orderedRecordings[length / 2];
       return (firstNumber + secondNumber) / 2;
     } else {
       // or return the middle number of the sorted recordings
-      return orderedRecordings[Math.floor(length / 2)]
+      return orderedRecordings[Math.floor(length / 2)];
     }
   }
-  
+
   /*
     Calculates the modal value of the recordings (there could be one or more),
     or returns null if there are no recordings
   */
-  mode():null | Array<number> {
+  mode(): null | Array<number> {
     if (this.recordings.length === 0) return null;
     // Used to count how many times each number occurs.
     const counts = this.counts();
 
     // Sort the counts by their occurrence
-    const sortFunction = (
-      a:[string, number],
-      b:[string, number]
-    ) => {
-      return a[1] - b[1]
+    const sortFunction = (a: [string, number], b: [string, number]) => {
+      return a[1] - b[1];
     };
     const sortedCounts = Object.entries(counts).sort(sortFunction);
     const numberOfKeys = Object.keys(sortedCounts).length - 1;
     const occurrence = sortedCounts[numberOfKeys][1];
-    const findMostCounted = (a:[string, number]) => a[1] === occurrence;
-    const convertToNumber = (a:[string, number]) => {
-      const countAsString:string = a[0]; 
+    const findMostCounted = (a: [string, number]) => a[1] === occurrence;
+    const convertToNumber = (a: [string, number]) => {
+      const countAsString: string = a[0];
       return parseInt(countAsString);
     };
     return sortedCounts.filter(findMostCounted).map(convertToNumber);
@@ -95,13 +91,13 @@ class Measure {
     Returns an object with keys of how many times each value has occurred
   */
   counts(): object {
-    const counts:CountObject = {}
+    const counts: CountObject = {};
 
     // Loop through the recordings to count occurrences
-    this.recordings.forEach((recording:number) => {
+    this.recordings.forEach((recording: number) => {
       if (!counts.hasOwnProperty(recording)) {
-      // Note the first occurrence
-      counts[recording] = 1;
+        // Note the first occurrence
+        counts[recording] = 1;
       } else {
         // Note the next occurrence
         counts[recording]++;
@@ -110,24 +106,27 @@ class Measure {
     return counts;
   }
 
-  variance():null|number {
-    const mean:null|number = this.mean();
+  variance(): null | number {
+    const mean: null | number = this.mean();
     if (mean === null) return null;
-    const deviations = this.recordings.map((recording:number) => {
+    const deviations = this.recordings.map((recording: number) => {
       const deviation = recording - mean;
       return deviation * deviation;
-    })
-    const sumFunction = (total:number, recording:number) => total + recording;
-    const sumOfDeviations = deviations.reduce(sumFunction,0);
-    const divisor = this.type === 'population' ? this.recordings.length : this.recordings.length - 1;
-    return (sumOfDeviations / divisor);
+    });
+    const sumFunction = (total: number, recording: number) => total + recording;
+    const sumOfDeviations = deviations.reduce(sumFunction, 0);
+    const divisor =
+      this.type === 'population'
+        ? this.recordings.length
+        : this.recordings.length - 1;
+    return sumOfDeviations / divisor;
   }
 
   /*
     Returns the standard deviation of the recordings
   */
-  stdev():null|number {
-    const variance:null|number = this.variance();
+  stdev(): null | number {
+    const variance: null | number = this.variance();
     if (variance === null) return null;
     return Math.sqrt(variance);
   }
@@ -135,15 +134,13 @@ class Measure {
   /*
     Returns the standard score of a value
   */
-  zscore(value:number):null|number {
+  zscore(value: number): null | number {
     if (this.recordings.length === 0) return null;
     const mean = this.mean();
     const stdev = this.stdev();
     if (!mean || !stdev) return null;
     return (value - mean) / stdev;
   }
-
-};
+}
 
 export default Measure;
-
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,4 @@
+import assert from 'assert';
 import Measure from '../src';
 
 describe('Measure', () => {
@@ -5,14 +6,14 @@ describe('Measure', () => {
     it('should add the entry to the recordings array', () => {
       const measure = new Measure();
       measure.record(4);
-      expect(measure.recordings).toStrictEqual([4]);
+      assert.deepEqual(measure.recordings, [4]);
     });
 
     describe('when the value is an array', () => {
       it('should add all of the items in the array to the recordings', () => {
         const measure = new Measure();
         measure.record([4, 5, 6]);
-        expect(measure.recordings).toStrictEqual([4, 5, 6]);
+        assert.deepEqual(measure.recordings, [4, 5, 6]);
       });
     });
   });
@@ -21,7 +22,7 @@ describe('Measure', () => {
     describe('when there are no recordings yet', () => {
       it('should return null', () => {
         const measure = new Measure();
-        expect(measure.mean()).toEqual(null);
+        assert.equal(measure.mean(), null);
       });
     });
 
@@ -29,8 +30,8 @@ describe('Measure', () => {
       it('should return the average value of the recordings', () => {
         const measure = new Measure();
         const values = [1, 2, 3, 4];
-        values.forEach(v => measure.record(v));
-        expect(measure.mean()).toEqual(2.5);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.mean(), 2.5);
       });
     });
   });
@@ -39,7 +40,7 @@ describe('Measure', () => {
     describe('when there are no recordings yet', () => {
       it('should return null', () => {
         const measure = new Measure();
-        expect(measure.median()).toEqual(null);
+        assert.equal(measure.median(), null);
       });
     });
 
@@ -47,8 +48,8 @@ describe('Measure', () => {
       it('should return the average value of the 2 middle recordings', () => {
         const measure = new Measure();
         const values = [1, 2, 3, 4, 5, 6];
-        values.forEach(v => measure.record(v));
-        expect(measure.median()).toEqual(3.5);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.median(), 3.5);
       });
     });
 
@@ -56,8 +57,8 @@ describe('Measure', () => {
       it('should return the median value of the recordings', () => {
         const measure = new Measure();
         const values = [1, 2, 3, 4, 5, 6, 7];
-        values.forEach(v => measure.record(v));
-        expect(measure.median()).toEqual(4);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.median(), 4);
       });
     });
   });
@@ -66,7 +67,7 @@ describe('Measure', () => {
     describe('when there are no recordings yet', () => {
       it('should return null', () => {
         const measure = new Measure();
-        expect(measure.mode()).toEqual(null);
+        assert.equal(measure.mode(), null);
       });
     });
 
@@ -74,8 +75,8 @@ describe('Measure', () => {
       it('should return an array of those 2 recordings', () => {
         const measure = new Measure();
         const values = [1, 2, 2, 3, 3, 4, 5];
-        values.forEach(v => measure.record(v));
-        expect(measure.mode()).toEqual([2, 3]);
+        values.forEach((v) => measure.record(v));
+        assert.deepEqual(measure.mode(), [2, 3]);
       });
     });
 
@@ -83,8 +84,8 @@ describe('Measure', () => {
       it('should return an array with just that one recording', () => {
         const measure = new Measure();
         const values = [1, 2, 2, 3, 3, 3, 4, 5];
-        values.forEach(v => measure.record(v));
-        expect(measure.mode()).toEqual([3]);
+        values.forEach((v) => measure.record(v));
+        assert.deepEqual(measure.mode(), [3]);
       });
     });
   });
@@ -93,8 +94,8 @@ describe('Measure', () => {
     it('should return an object with occurrences counts for the values', () => {
       const measure = new Measure();
       const values = [1, 2, 2, 3, 3, 3, 4, 5];
-      values.forEach(v => measure.record(v));
-      expect(measure.counts()).toEqual({
+      values.forEach((v) => measure.record(v));
+      assert.deepEqual(measure.counts(), {
         '1': 1,
         '2': 2,
         '3': 3,
@@ -108,7 +109,7 @@ describe('Measure', () => {
     describe('when there are no recordings yet', () => {
       it('should return null', () => {
         const measure = new Measure();
-        expect(measure.variance()).toEqual(null);
+        assert.equal(measure.variance(), null);
       });
     });
 
@@ -116,8 +117,8 @@ describe('Measure', () => {
       it('should return the sample variance of the recordings', () => {
         const measure = new Measure();
         const values = [1, 2, 2, 3, 3, 3, 4, 5];
-        values.forEach(v => measure.record(v));
-        expect(measure.variance()).toEqual(1.5535714285714286);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.variance(), 1.5535714285714286);
       });
     });
 
@@ -125,8 +126,8 @@ describe('Measure', () => {
       it('should return the population variance', () => {
         const measure = new Measure({ type: 'population' });
         const values = [1, 2, 2, 3, 3, 3, 4, 5];
-        values.forEach(v => measure.record(v));
-        expect(measure.variance()).toEqual(1.359375);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.variance(), 1.359375);
       });
     });
   });
@@ -135,7 +136,7 @@ describe('Measure', () => {
     describe('when there are no recordings yet', () => {
       it('should return null', () => {
         const measure = new Measure();
-        expect(measure.stdev()).toEqual(null);
+        assert.equal(measure.stdev(), null);
       });
     });
 
@@ -143,8 +144,8 @@ describe('Measure', () => {
       it('should return the sample standard deviation of the recordings', () => {
         const measure = new Measure();
         const values = [1, 2, 2, 3, 3, 3, 4, 5];
-        values.forEach(v => measure.record(v));
-        expect(measure.stdev()).toEqual(1.246423454758225);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.stdev(), 1.246423454758225);
       });
     });
 
@@ -152,8 +153,8 @@ describe('Measure', () => {
       it('should return the population standard deviation', () => {
         const measure = new Measure({ type: 'population' });
         const values = [1, 2, 2, 3, 3, 3, 4, 5];
-        values.forEach(v => measure.record(v));
-        expect(measure.stdev()).toEqual(1.165922381636102);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.stdev(), 1.165922381636102);
       });
     });
   });
@@ -162,7 +163,7 @@ describe('Measure', () => {
     describe('when there are no recordings yet', () => {
       it('should return null', () => {
         const measure = new Measure();
-        expect(measure.zscore(5)).toEqual(null);
+        assert.equal(measure.zscore(5), null);
       });
     });
 
@@ -170,9 +171,9 @@ describe('Measure', () => {
       it('should return the z-score for a value', () => {
         const measure = new Measure();
         const values = [1, 2, 2, 3, 3, 3, 4, 5];
-        values.forEach(v => measure.record(v));
-        expect(measure.zscore(4)).toEqual(0.9025825017214731);
-        expect(measure.zscore(1)).toEqual(-1.5043041695357886);
+        values.forEach((v) => measure.record(v));
+        assert.equal(measure.zscore(4), 0.9025825017214731);
+        assert.equal(measure.zscore(1), -1.5043041695357886);
       });
     });
   });
